9ae3d46f67dc16683c7859d0be69f67bbdcb2091
kalpana-mind <152434265+kalpana-mind@users.noreply.github.com>
2024-06-19 22:42:24 -0400

    'Updating edit and delete product'

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const expressHbs = require('express-handlebars');
 
 const errorController = require('./controllers/error');
 const mongoConnect = require('./util/database').mongoConnect;
+const User = require('./models/user');
 
 //const User = require('./models/user');
 
@@ -29,12 +30,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-  // User.findByPk(1)
-  //   .then((user) => {
-  //     req.user = user;
-  //     next();
-  //   })
-  //   .catch((err) => console.log(err));
+  User.findById(1)
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((err) => console.log(err));
   next();
 });
 
--- models/product.js
+++ models/product.js
@@ -2,17 +2,25 @@ const mongodb = require('mongodb');
 const getDb = require('../util/database').getDb;
 
 class Product {
-  constructor(title, price, imageUrl, description) {
+  constructor(title, price, imageUrl, description, id) {
     this.title = title;
     this.price = price;
     this.description = description;
     this.imageUrl = imageUrl;
+    this._id = id ? new mongodb.ObjectId(id) : null;
   }
   save() {
     const db = getDb();
-    return db
-      .collection('products')
-      .insertOne(this)
+    let dbOp;
+    if (this._id) {
+      dbOp = db
+        .collection('products')
+        .updateOne({ _id: this._id }, { $set: this });
+    } else {
+      dbOp = db.collection('products').insertOne(this);
+    }
+
+    return dbOp
       .then((result) => console.log(result))
       .catch((err) => console.log(err));
   }
@@ -39,6 +47,17 @@ class Product {
       .then((product) => {
         console.log(product);
         return product;
+      })
+      .catch((err) => console.log(err));
+  }
+
+  static deletebyId(prodId) {
+    const db = getDb();
+    return db
+      .collection('products')
+      .deleteOne({ _id: new mongodb.ObjectId(prodId) })
+      .then((result) => {
+        console.log('Product is deleted');
       })
       .catch((err) => console.log(err));
   }
