bd759938976c741305d120ffd0e3beac44859edf
kalpana-mind <152434265+kalpana-mind@users.noreply.github.com>
2024-06-20 22:22:51 -0400

    "Updating cart and order functionality using mongodb"

--- app.js
+++ app.js
@@ -8,8 +8,6 @@ const errorController = require('./controllers/error');
 const mongoConnect = require('./util/database').mongoConnect;
 const User = require('./models/user');
 
-//const User = require('./models/user');
-
 const app = express();
 
 // app.engine(
@@ -32,11 +30,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) => {
   User.findById(1)
     .then((user) => {
-      req.user = user;
+      req.user = new User(user.name, user.email, user.cart, user._id);
       next();
     })
     .catch((err) => console.log(err));
-  next();
 });
 
 app.use('/admin', adminRoutes);
