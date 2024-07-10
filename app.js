ee2eb91f3ba4569c91122648f05fba8b69e64499
kalpana-mind <152434265+kalpana-mind@users.noreply.github.com>
2024-07-09 22:37:20 -0400

    fixed error in appjs

--- app.js
+++ app.js
@@ -28,7 +28,10 @@ const fileStorage = multer.diskStorage({
     cb(null, 'images');
   },
   filename: (Req, file, cb) => {
-    cb(null, new Date().toISOString() + '-' + file.originalname);
+    cb(
+      null,
+      new Date().toISOString().replace(/:/g, '-') + '-' + file.originalname
+    );
   },
 });
 
